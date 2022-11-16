b077419e8bf9ee80f162df29f20bd17cd6ad8259
9-Extra <77313011@qq.com>
2022-11-16 12:59:12 +0800

    实现了blog的提交和修改

--- src/main/java/cn/mango/mangoblog/restserver/BlogController.java
+++ src/main/java/cn/mango/mangoblog/restserver/BlogController.java
@@ -1,8 +1,73 @@
 package cn.mango.mangoblog.restserver;
 
-import org.springframework.web.bind.annotation.RestController;
+import cn.mango.mangoblog.entity.Blog;
+import cn.mango.mangoblog.entity.BlogOperation;
+import cn.mango.mangoblog.entity.ResultWrapper;
+import cn.mango.mangoblog.runtime.BlogServiceImpl;
+import cn.mango.mangoblog.utils.TokenUtils;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class BlogController {
+    @Autowired
+    private BlogServiceImpl BlogServiceImpl;
 
+    @GetMapping("/blog")
+    public ResultWrapper<Blog> getBlog(@RequestParam(value = "id", required = true) Long id) {
+        return BlogServiceImpl.getBlog(id);
+    }
+
+    //发布blog
+    @PostMapping("/post")
+    public ResultWrapper<Long> Post(@RequestBody BlogOperation operation, @RequestHeader(value = "authorization", required = false) String token) {
+        Optional<DecodedJWT> decodedJWT = TokenUtils.verify(token);//decodedJWT:校验后的token
+        if (decodedJWT.isEmpty()){
+            return new ResultWrapper<>(2, "token错误", 0L);
+        }
+
+        DecodedJWT decodedJWT1 = decodedJWT.get();
+        Long user_id = decodedJWT1.getClaim("user_id").asLong();//获取用户id
+
+        switch (operation.getOperation()){
+            case BlogOperation.OPERATION_NEW -> {
+                Blog blog = operation.getBlog();
+
+                if (blog == null){
+                    return new ResultWrapper<>(500, "博客不能为空", 0L);
+                }
+
+                blog.setId(user_id);
+
+                if (BlogServiceImpl.insertBlog(blog)){
+                    return new ResultWrapper<>(blog.getId());
+                } else {
+                    return new ResultWrapper<>(500, "上传博客失败", 0L);
+                }
+            }
+            case BlogOperation.OPERATION_MODIFY -> {
+                Blog blog = operation.getBlog();
+                if (blog != null && BlogServiceImpl.updateBlog(blog)){
+                    return new ResultWrapper<>(blog.getId());
+                } else {
+                    return new ResultWrapper<>(500, "修改博客失败", 0L);
+                }
+            }
+
+            case BlogOperation.OPERATION_DELETE -> {
+                if (BlogServiceImpl.deleteBlog(operation.getBlog_id())){
+                    return new ResultWrapper<>(500, "删除博客失败", 0L);
+                } else {
+                    return new ResultWrapper<>(operation.getBlog_id());
+                }
+            }
+
+            default -> {
+                return new ResultWrapper<>(400, "无效操作", 0L);
+            }
+        }
+    }
 }
---
+++ src/main/java/cn/mango/mangoblog/runtime/BlogServiceImpl.java
@@ -0,0 +1,49 @@
+package cn.mango.mangoblog.runtime;
+
+import cn.mango.mangoblog.entity.Blog;
+import cn.mango.mangoblog.entity.ResultWrapper;
+import cn.mango.mangoblog.mapper.BlogMapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Primary
+@Service
+public class BlogServiceImpl {
+    @Autowired
+
+    private BlogMapper blogMapper;
+    //获取Blog
+    public ResultWrapper<Blog> getBlog(Long id){
+        //查询构造器
+        QueryWrapper<Blog> qw = new QueryWrapper<>();
+        qw.eq("id", id);
+        List<Blog> result = blogMapper.selectList(qw);
+        if (result.isEmpty()){
+            return new ResultWrapper<>(400, "Not found", null);
+        }
+        return new ResultWrapper<>(result.get(0));
+    }
+
+    //插入blog并返回blog_id
+    public boolean insertBlog(Blog blog){
+        return blogMapper.insert(blog) == 1;//返回值1表示插入成功
+    }
+
+    //更新blog并返回blog_id
+    public boolean updateBlog(Blog blog){
+        UpdateWrapper<Blog> blogUpdateWrapper = new UpdateWrapper<>(blog);
+        blogUpdateWrapper.set("id", blog.getId()).set("author_id", blog.getAuthor_id());
+        blogMapper.update(blog, blogUpdateWrapper);
+        return blogMapper.updateById(blog) == 1;
+    }
+
+    //删除blog
+    public boolean deleteBlog(Long id){
+        return blogMapper.deleteById(id) == 1;
+    }
+}
