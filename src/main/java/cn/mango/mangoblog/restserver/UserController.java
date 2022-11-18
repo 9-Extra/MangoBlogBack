667186f7ffa877ff497ed3763a71798f5bbe3d9b
9-Extra <77313011@qq.com>
2022-11-18 18:56:10 +0800

    修复无法检查重复登录的bug

--- src/main/java/cn/mango/mangoblog/restserver/UserController.java
+++ src/main/java/cn/mango/mangoblog/restserver/UserController.java
@@ -1,18 +1,18 @@
 package cn.mango.mangoblog.restserver;
 
-import cn.mango.mangoblog.entity.*;
-import cn.mango.mangoblog.runtime.UserServiceImpl;
+import cn.mango.mangoblog.entity.LoginMessage;
+import cn.mango.mangoblog.entity.ResultWrapper;
+import cn.mango.mangoblog.entity.User;
+import cn.mango.mangoblog.entity.VerifyResult;
 import cn.mango.mangoblog.mapper.UserMapper;
+import cn.mango.mangoblog.runtime.UserServiceImpl;
 import cn.mango.mangoblog.utils.TokenUtils;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -34,7 +34,7 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResultWrapper<String> login(@RequestParam(value = "authorization", required = false) String token, @RequestBody LoginMessage loginMessage) {
+    public ResultWrapper<String> login(@RequestHeader(value = "authorization", required = false) String token, @RequestBody LoginMessage loginMessage) {
         ResultWrapper<VerifyResult> verifyResult = TokenUtils.Verify(token);//获取验证结果
         VerifyResult verifyresultData = verifyResult.getData();
         if (verifyresultData != null) {
@@ -43,37 +43,36 @@ public class UserController {
                 return new ResultWrapper<>(500, "重复登录", token);
             }
         }
-            return userService.login_check(loginMessage.id, loginMessage.password);
+        return userService.login_check(loginMessage.id, loginMessage.password);
     }
 
     @PostMapping("/upgrade")
-    public ResultWrapper<Long> UpgradeUser(@RequestParam(value = "id",required = true)Long id,@RequestHeader(value = "authorization",required = true)String token){
+    public ResultWrapper<Long> UpgradeUser(@RequestParam(value = "id", required = true) Long id, @RequestHeader(value = "authorization", required = true) String token) {
         ResultWrapper<VerifyResult> verifyResult = TokenUtils.Verify(token);//获取验证结果
         VerifyResult verifyresultData = verifyResult.getData();
         if (verifyresultData == null)
             return new ResultWrapper<>(verifyResult.getCode(), verifyResult.getMessage(), null);
         Integer privilege = verifyresultData.getPrivilege();
-        if(privilege!=2)
-            return new ResultWrapper<>(2,"invalid token",0L);
-        if(userService.UpdateUserPrivilege(id,1)){
-            return new ResultWrapper<>(0,"Success",null);
-        }
-        else return new ResultWrapper<>(400,"Invalid id",0L);
+        if (privilege != 2)
+            return new ResultWrapper<>(2, "invalid token", 0L);
+        if (userService.UpdateUserPrivilege(id, 1)) {
+            return new ResultWrapper<>(0, "Success", null);
+        } else return new ResultWrapper<>(400, "Invalid id", 0L);
     }
 
     @PostMapping("/me")//用户查看自己的信息
-    public ResultWrapper<User> getMySelf(@RequestHeader(value = "authorization",required = true)String token){
-        ResultWrapper<VerifyResult> verifyResultResultWrapper=TokenUtils.Verify(token);
-        if(verifyResultResultWrapper.getCode()!=0)
+    public ResultWrapper<User> getMySelf(@RequestHeader(value = "authorization", required = true) String token) {
+        ResultWrapper<VerifyResult> verifyResultResultWrapper = TokenUtils.Verify(token);
+        if (verifyResultResultWrapper.getCode() != 0)
             return new ResultWrapper<>(verifyResultResultWrapper.getCode(), verifyResultResultWrapper.getMessage(), null);
-        Long user_id=verifyResultResultWrapper.getData().getId();
+        Long user_id = verifyResultResultWrapper.getData().getId();
 //        List<Blog> blogList= blogMapper.selectList(Wrappers.<Blog>lambdaQuery().eq(Blog::getId,blog_id).eq(Blog::getAuthorid,user_id));
-        List<User> UserList=userMapper.selectList(Wrappers.<User>lambdaQuery().eq(User::getId,user_id));
-        if(UserList.isEmpty()){
-            return new ResultWrapper<>(2,"Invalid token",null);
+        List<User> UserList = userMapper.selectList(Wrappers.<User>lambdaQuery().eq(User::getId, user_id));
+        if (UserList.isEmpty()) {
+            return new ResultWrapper<>(2, "Invalid token", null);
         }
-        User user=UserList.get(0);
+        User user = UserList.get(0);
         user.setPassword(null);
-        return new ResultWrapper<>(0,"Success",user);
+        return new ResultWrapper<>(0, "Success", user);
     }
 }
