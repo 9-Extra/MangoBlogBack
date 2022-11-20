24905fe774536aaddeb7266d3a943d7108bbb5e3
9-Extra <77313011@qq.com>
2022-11-20 13:12:59 +0800

    修复上传函数文件名的错误

--- src/main/java/cn/mango/mangoblog/utils/FileUtils.java
+++ src/main/java/cn/mango/mangoblog/utils/FileUtils.java
@@ -20,35 +20,30 @@ public class FileUtils {
     public static String Upload(MultipartFile file, Long blog_id, Long user_id) {//放入/image/upload/user_id/blog_id下
         //上传路径保存设置
         //获得SpringBoot当前项目的路径：System.getProperty("user.dir")
-        String root = System.getProperty("user.dir") + "/image";
+        String system_root = System.getProperty("user.dir") + "/image";
         String path;
         if (blog_id != null) {
-            path = root + "/upload/" + user_id + "/" + blog_id;
+            path = String.format("/upload/%d/%d/%s", user_id, blog_id, file.getOriginalFilename());
         } else {
-            path = root +  "/upload/" + user_id;
+            path = String.format("/upload/%d/%s", user_id, file.getOriginalFilename());
         }
-        File realPath = new File(path);
+        File file_save = new File(system_root, path);
         //路径不存在就创建路径
-        if (!realPath.exists()) {
-            realPath.mkdirs();
-        }
+        file_save.mkdirs();
+
         //解决文件名字问题：我们使用uuid;
         //String filename = "pg-" + UUID.randomUUID().toString().replaceAll("-", "") + ".jpg";//后缀
 
-        //直接使用用户上传时的文件名称
-        String filename = file.getName();
-
         //存入新文件
         try {
-            File file_save = new File(realPath, filename);
             file.transferTo(file_save);
             //上传文件地址
-            System.out.println("上传文件保存地址：" + file_save.getName());
+            System.out.println("上传文件保存地址：" + file_save.getAbsolutePath());
         } catch (IOException e) {
             return null;
         }
         System.out.println("============上传成功============");
-        return String.format("/upload/%d/%d/%s", user_id, blog_id, filename);
+        return path;
     }
 
     public static void Delete(String spath) {//删除图片及文件夹，spath指明文件时删除文件，否则删除文件夹,spath为文件真实路径
